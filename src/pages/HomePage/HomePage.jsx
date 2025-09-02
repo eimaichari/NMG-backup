04c553cd24f1af3c0908ee95662e21e6452c04f1
Jeremie Kazadi <jeremiahkazadi9@gmail.com>
2025-09-02 20:44:00 +0200

    img

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -28,7 +28,7 @@ const HomePage = () => {
               We set new standards for quality, reliability, and innovation by offering essential services under one unified,
               professional brand.
             </p>
-            <img src="../src/assets/images/lunch-box.jpg" alt="Team" />
+            <img src="lunch-box.jpg" alt="Team" />
           </div>
           <div className={styles.rightSection}>
             <h1 className={styles.gradientText}>WELCOME TO NMG ZEMBETA</h1>
