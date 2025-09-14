a2d83a5b6da1c103db5004eb71c47a4b7162636a
Jeremie Kazadi <jeremiahkazadi9@gmail.com>
2025-09-14 13:03:05 +0200

    double check footer fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,20 +21,34 @@ const Footer = () => {
           <h4>Contact Info</h4>
           <p>Email: nasiphizembeta@gmail.com</p>
           <p>Phone: (+27)73 974-0331</p>
+          <div className={styles.footerSocials}>
+            <a href="https://www.facebook.com/share/1F33u4amC7/" target="_blank" rel="noopener noreferrer">
+              <img src="../srs/assets/images/apple-touch-icon-facebook.png" alt="Facebook icon" />
+            </a>
+            <a href="https://www.instagram.com/nmgm439?utm_source=qr&igsh=NmZyZW9rNnZuaGd1" target="_blank" rel="noopener noreferrer">
+              <img src="../srs/assets/images/apple-touch-icon-ig.png" alt="Instagram icon" />
+            </a>
+            <a href="https://www.tiktok.com/@nasie325?_t=ZS-8z96uUQ1TeB&_r=1" target="_blank" rel="noopener noreferrer">
+              <img src="../srs/assets/images/apple-touch-icon-tiktok.png" alt="TikTok icon" />
+            </a>
+            <a href="https://youtube.com/@nasie6302/community?si=Oglk3z9MhHV0yr3L" target="_blank" rel="noopener noreferrer">
+              <img src="../srs/assets/images/apple-touch-icon-youtube.png" alt="YouTube icon" />
+            </a>
+          </div>
         </div>
       </div>
       <div className={styles.footerBottom}>
-       <p>
-        &copy; {new Date().getFullYear()} NMG-Zembeta. All rights reserved. Built By{" "}
-        <a
-          href="https://www.linkedin.com/in/jeremie-kazadi-013a55372?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: "inherit", textDecoration: "underline" }}
-        >
-          JAY_K
-        </a>
-      </p>
+        <p>
+          &copy; {new Date().getFullYear()} NMG-Zembeta. All rights reserved. Built By{" "}
+          <a
+            href="https://www.linkedin.com/in/jeremie-kazadi-013a55372?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "inherit", textDecoration: "underline" }}
+          >
+            JAY_K
+          </a>
+        </p>
       </div>
     </footer>
   );
