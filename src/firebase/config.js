367875ca63e5d93a4e2893fdcd75b1f6ff2b77d1
Jeremie Kazadi <jeremiahkazadi9@gmail.com>
2026-06-22 11:52:03 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -16,8 +16,7 @@ const firebaseConfig = {
   appId:             import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-console.log('Firebase projectId:', firebaseConfig.projectId);
-
+console.log(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 
 export const auth    = getAuth(app);
